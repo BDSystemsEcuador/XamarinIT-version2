aa75fd4f860778077d177246aa5f914fe61441ac
BDSystems-Ecuador <byrondanipm@gmail.com>
2021-04-13 15:12:31 -0500

    version 2

---
+++ ProyectoITXamarin/ProyectoITXamarin/Data/Factura.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace ProyectoITXamarin.Data
+{
+    public class Factura : ModelObject
+    {
+        int id;
+        string nameEmpresa;
+        string fecha;
+        string name;
+        public Factura()
+        {
+            this.id = 0;
+            this.nameEmpresa = "";
+            this.fecha = "";
+            this.name = "";
+        }
+        public Factura(int id, string nameEmpresa, string fecha, string name)
+        {
+            this.id = id;
+            this.nameEmpresa = nameEmpresa;
+            this.fecha = fecha;
+            this.name = name;
+        }
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                if (id != value)
+                {
+                    id = value;
+                    RaisePropertyChanged("Id");
+                }
+            }
+        }
+        public string NameEmpresa
+        {
+            get { return nameEmpresa; }
+            set
+            {
+                if (nameEmpresa != value)
+                {
+                    nameEmpresa = value;
+                    RaisePropertyChanged("NameEmpresa");
+                }
+            }
+        }
+        public string Fecha
+        {
+            get { return fecha; }
+            set
+            {
+                if (fecha != value)
+                {
+                    fecha = value;
+                    RaisePropertyChanged("Fecha");
+                }
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    RaisePropertyChanged("Name");
+                }
+            }
+        }
+    }
+    public class ModelObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged(string nameEmpresa)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(nameEmpresa));
+        }
+    }
+}
---
+++ ProyectoITXamarin/ProyectoITXamarin/Data/ViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+using DevExpress.XamarinForms.Charts;
+
+namespace ProyectoITXamarin
+{
+    public class ViewModel
+    {
+        public List<Venta> Ventas { get; set; }
+        readonly Color[] palette;
+        public Color[] Palette => palette;
+
+        public ViewModel()
+        {
+            List<Venta> Ventas = new List<Venta>();
+            TraerDatosCharts();
+            palette = PaletteLoader.LoadPalette("#25a966", "#F13D45", "#45B6F1", "#F7EC42", "#975ba5", "#f45a4e");
+        }
+        static class PaletteLoader
+        {
+            public static Color[] LoadPalette(params string[] values)
+            {
+                Color[] colors = new Color[values.Length];
+                for (int i = 0; i < values.Length; i++)
+                    colors[i] = Color.FromHex(values[i]);
+                return colors;
+            }
+        }
+
+        public async void TraerDatosCharts()
+        {
+            var request = new HttpRequestMessage();
+            request.RequestUri = new Uri("https://api.jsonbin.io/b/6074fbffee971419c4d7d967");
+            request.Method = HttpMethod.Get;
+            request.Headers.Add("Accpet", "application/json");
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                var resultado = JsonConvert.DeserializeObject<List<Venta>>(content);
+                //Ventas. = resultaxdoñ
+                this.Ventas = resultado;
+            }
+        }
+    }
+
+    public class Venta
+    {
+        public string NameEmpresa { get; set; }
+        public int Cantidad { get; set; }
+
+        public Venta(int cantidad, string nameEmpresa)
+        {
+            this.NameEmpresa = nameEmpresa;
+            this.Cantidad = cantidad;
+        }
+    }
+    public class CustomColorizer : ICustomPointColorizer
+    {
+        Color ICustomPointColorizer.GetColor(ColoredPointInfo info)
+        {
+            
+            return Color.FromHex("#9AE4F3");
+        }
+        public ILegendItemProvider GetLegendItemProvider()
+        {
+            return null;
+        }
+    }
+}
